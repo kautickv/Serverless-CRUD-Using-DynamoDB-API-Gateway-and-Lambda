1f4b9f552964161a69f48067a405d257258b7454
kautickv <kautickv29@gmail.com>
2022-12-22 11:56:46 -0600

    Added all form components in html file

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayAddStudentForm(){
+    // This function will display the form to the user to add a new student.
+    var htmlBlock = ''
+    document.getElementById("addStudentDiv").innerHTML= htmlBlock;
+
+}
+
+function displayUpdateStudentForm(){
+
+}
+
+function displayDeleteStudentForm(){
+
+}
+
+function listAllStudent(){
+
+}
